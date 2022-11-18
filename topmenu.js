4ac93cf04eebb8808c30c55bdebfe7791e15b39a
BIT@SDUB <bit@bib.sdu.dk>
2022-11-18 15:15:30 +0100

    REPO as attribute

--- topmenu.js
+++ topmenu.js
@@ -26,16 +26,17 @@
  *  @copyright http://www.gnu.org/licenses/lgpl.txt LGPL version 3
  *  @author    Erik Bachmann <ErikBachmann@ClicketyClick.dk>
  *  @since     2022-11-18T10:22:57 / Erik Bachmann
- *  @version   2022-11-18T10:39:45 / Erik Bachmann
+ *  @version   2022-11-18T15:13:24 / Erik Bachmann
  */
 // ClicketyClick.github.io/docs/incl.js
 console.log("ClicketyClick.github.io/docs/topmenu.js loading");
 
 var SITE  = "bit-sdub";
+var HEAD =  "<div align='right'>";
 
 console.log("SITE=["+SITE+"] REPO=["+REPO+"]");
 
-
+// Activate DEMO button
 if ( typeof DEMO !== 'undefined' ) {
 	DEMO = "<button onclick=\"location.href='https://"
     + SITE
@@ -48,6 +49,7 @@ if ( typeof DEMO !== 'undefined' ) {
 	DEMO = "";
 }
 
+// Activate RELEASE button
 if ( typeof RELEASE !== 'undefined' ) {
 	RELEASE = "<button onclick=\"location.href='https://"
     + SITE
@@ -60,7 +62,9 @@ if ( typeof RELEASE !== 'undefined' ) {
 	RELEASE = "";
 }
 
-var HEAD =  "<div align='right'>"
+// Only repos can have home, up etc. buttons
+if ( typeof REPO !== 'undefined' ) {
+	HEAD	= HEAD
 
 // Demo
 	+ DEMO
@@ -80,10 +84,14 @@ var HEAD =  "<div align='right'>"
   + "<button onclick=\"location.href='..'\" type='button' title='Up'>&#x2303;</button>"
 // Home
   + "<button onclick=\"location.href='/'\" type='button' title='Home'>&#x1F3E0;</button>"
-  + "</div>";
+} else {
+	HEAD	+= ".";
+}
+
+HEAD	+= "</div>";
 
 var FOOTER = "<hr>&copy;2022 Clicketyclick.dk";
 document.getElementsByTagName("body")[0].innerHTML = HEAD + document.getElementsByTagName("body")[0].innerHTML + FOOTER;
 
-console.log("ClicketyClick.github.io/docs/topmenu.js  Header inserted");
+console.log("ClicketyClick.github.io/docs/topmenu.js  Header and footer inserted");
 console.log("ClicketyClick.github.io/docs/topmenu.js  ended");
